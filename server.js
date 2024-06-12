e658326357a103b7dbfae275dfe26a957d3a6295
adesekosamdev <adesekosam@gmail.com>
2024-06-12 17:58:16 +0000

    https changes

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const budgetRoutes = require('./routes/budget');
 const authRoutes = require('./routes/auth');
 require('dotenv').config();
 const cors = require('cors');
+const https = require('https');
+const http = require('http');
+const fs = require('fs');
 
 const app = express();
 
@@ -20,10 +23,22 @@ app.use('/api/users', userRoutes);
 app.use('/api/budgets', budgetRoutes);
 app.use('/api/auth', authRoutes);
 
+// Load SSL certificate and key
+const options = {
+  key: fs.readFileSync('/etc/letsencrypt/live/web-02.bazzyx.tech/privkey.pem'),
+  cert: fs.readFileSync('/etc/letsencrypt/live/web-02.bazzyx.tech/fullchain.pem')
+};
+
 // Sync database and start server
 const PORT = process.env.PORT || 5000;
 sequelize.sync({ alter: true }).then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
+  https.createServer(options, app).listen(PORT, () => {
+    console.log(`HTTPS Server started on port ${PORT}`);
   });
 });
+
+// Redirect HTTP to HTTPS
+http.createServer((req, res) => {
+  res.writeHead(301, { "Location": "https://" + req.headers['host'] + req.url });
+  res.end();
+}).listen(80);
