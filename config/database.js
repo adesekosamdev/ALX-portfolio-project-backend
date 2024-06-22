d956968e67288bdff9b4ed944700eeca3225fd82
pizza man <adesekosam.dev@gmail.com>
2024-06-22 14:57:10 +0300

    Configure PostgreSQL with SSL

--- config/database.js
+++ config/database.js
@@ -1,10 +1,33 @@
 // config/database.js
+// const { Sequelize } = require('sequelize');
+// require('dotenv').config();
+
+// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+//   host: process.env.DB_HOST,
+//   dialect: 'postgres'
+// });
+
+// module.exports = sequelize;
+
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-  host: process.env.DB_HOST,
-  dialect: 'postgres'
+const databaseUrl = process.env.DATABASE_URL;
+
+if (!databaseUrl) {
+  throw new Error('DATABASE_URL environment variable is not set');
+}
+
+const sequelize = new Sequelize(databaseUrl, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // For self-signed certificates, this may need to be false
+    },
+  },
+  logging: false, // Disable logging; default: console.log
 });
 
 module.exports = sequelize;
